2066f59e403037672f81037b12795f4c80a5580c
Nikita <gorbatenko.nik@bk.ru>
2023-03-05 22:37:45 +0700

    вывод чет/нечет

---
+++ Задача 6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number % 2;
+if (a == 0)
+{
+    Console.WriteLine($"{number} - четное число");
+}
+else
+{
+     Console.WriteLine($"{number} - нечетное число");
+}
+
