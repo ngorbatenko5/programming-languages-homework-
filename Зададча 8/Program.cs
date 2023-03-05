39b166333fd64f9dd1f7b297113ddf1251a3bea7
Nikita <gorbatenko.nik@bk.ru>
2023-03-05 22:50:04 +0700

    четные от 1 до N

---
+++ Зададча 8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int evenNumber = 2;
+if (number > 1)
+{
+    while (evenNumber <= number)
+    {
+        Console.Write(evenNumber+ " ");
+        evenNumber = evenNumber + 2;
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели ненатуральное число:");
+}
