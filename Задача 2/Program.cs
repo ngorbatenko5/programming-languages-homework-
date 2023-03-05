c34a15057071db0640df939a10603dbf7bce7cf4
Nikita <gorbatenko.nik@bk.ru>
2023-03-05 21:43:00 +0700

    вывод мин и мах

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int max = 0;
+int min = 0;
+Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2)
+{
+    max = number1;
+    min = number2;
+}
+else
+{
+    max = number2;
+    min = number1;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
+
